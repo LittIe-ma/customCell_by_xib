ab2935d2e72e6d96470ff4fa4969dacc56516a4b
LittIe-ma <yasudaM84@gmail.com>
2022-02-19 15:04:31 +0900

    Initial Commit

---
+++ customCell_by_xib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  customCell_by_xib
+//
+//  Created by Masato Yasuda on 2022/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
