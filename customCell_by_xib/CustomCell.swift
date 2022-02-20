09f9ecb69ffa18af215be3896ed16a018e482544
LittIe-ma <yasudaM84@gmail.com>
2022-02-20 23:57:05 +0900

    initial commit

---
+++ customCell_by_xib/CustomCell.swift
@@ -0,0 +1,27 @@
+//
+//  customCell.swift
+//  customCell_by_xib
+//
+//  Created by Masato Yasuda on 2022/02/19.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    static var className: String { String(describing: CustomCell.self) }
+
+    //MARK: - PARTS
+    @IBOutlet weak var postFrameView: UIView! {
+        didSet {
+            postFrameView.backgroundColor = .systemBackground
+            postFrameView.layer.borderWidth = 1
+            postFrameView.layer.borderColor = UIColor.systemGray6.cgColor
+            postFrameView.layer.shadowOffset = CGSize(width: 1.0, height: 2.0)
+            postFrameView.layer.shadowColor = UIColor.black.cgColor
+            postFrameView.layer.shadowOpacity = 0.4
+            postFrameView.layer.shadowRadius = 4
+            postFrameView.layer.cornerRadius = 10
+        }
+    }
+}
--- customCell_by_xib/ViewController.swift
+++ customCell_by_xib/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.register(UINib(nibName: CustomCell.className, bundle: nil), forCellReuseIdentifier: CustomCell.className)
+        }
     }
 
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomCell.className, for: indexPath) as! CustomCell
+        return cell
+    }
+}
